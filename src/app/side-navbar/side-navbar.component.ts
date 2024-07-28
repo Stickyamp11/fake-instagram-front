ee22c3f1f55cfd7f5ec9e09b693a4af3bac33c43
C# Shark <stickyamp@hotmail.com>
2024-07-28 12:05:32 +0200

    completed feed for user + responsive

--- src/app/side-navbar/side-navbar.component.ts
+++ src/app/side-navbar/side-navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ResponsiveHidder1100Directive } from '../responsive-hidder.directive';
+import { ResponsiveHidder700Directive } from '../responsive-hidder-700.directive';
 
 @Component({
   selector: 'app-side-navbar',
   standalone: true,
-  imports: [ResponsiveHidder1100Directive],
+  imports: [ResponsiveHidder1100Directive,ResponsiveHidder700Directive],
   templateUrl: './side-navbar.component.html',
   styleUrl: './side-navbar.component.scss'
 })
